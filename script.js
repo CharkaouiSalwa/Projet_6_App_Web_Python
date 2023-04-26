7d4ea83f0ad589c9d851f119fc6075609eab20d4
CharkaouiSalwa <salwacharkaoui@gmail.com>
2023-04-26 18:08:19 +0200

    ajouter le code des buttons de caroussels

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ async function getBestMovieData() { // pour récupérer les données de tous les
 }
 
 
-/*<button  class="play-button" onclick="ouvrirModal()" ><i class="fa fa-play"></i> Play</button>*/
+// pour afficher le meileur film
 fetch('http://localhost:8000/api/v1/titles/?sort_by=-imdb_score')
   .then(response => response.json())
   .then(data => {
@@ -44,37 +44,80 @@ fetch('http://localhost:8000/api/v1/titles/?sort_by=-imdb_score')
   })
   .catch(error => console.error(error));
 
-
+// pour affiche les septs films mieux notés
 var page = 1;
 let allFilms = [];
-while (page < 3)
-{
-    fetch('http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page='+page, {async: false})
-      .then(response => response.json())
-      .then(data => {
-        let topFilms = data.results;
-        allFilms = allFilms.concat(topFilms);
-        if (allFilms.length == 10){
-            allFilms = allFilms.slice(1,8);
-            var element = document.getElementById("top-films-list");
-            element.classList.add("active");
-            const topFilmsList = document.getElementById('top-films-list');
-            allFilms.forEach(film => {
-              const filmCard = document.getElementById('film-card');
-              filmCard.innerHTML += `
-                    <div class="top-film-img">
-                        <img src="${film.image_url}" alt="${film.title}" onclick="ouvrirModal('${film.url}')" >
-              `;
-              topFilmsList.appendChild(filmCard);
-            });
+while (page < 3) {
+  fetch('http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page=' + page, { async: false })
+    .then(response => response.json())
+    .then(data => {
+      let topFilms = data.results;
+      allFilms = allFilms.concat(topFilms);
+      if (allFilms.length == 10) {
+        allFilms = allFilms.slice(1, 8);
+        const topFilmsList = document.getElementById('top-films-list');
+        allFilms.forEach(film => {
+          const filmCard = document.createElement('div');
+          filmCard.classList.add('top-film-img');
+          filmCard.innerHTML = `
+            <img src="${film.image_url}" alt="${film.title}" onclick="ouvrirModal('${film.url}')" >
+          `;
+          topFilmsList.appendChild(filmCard);
+        });
+        const hiddenFilms = document.querySelectorAll('.top-film-img:nth-child(n+6)');
+        hiddenFilms.forEach(film => {
+          film.classList.add('hidden');
+        });
+      }
+    })
+    .catch(error => console.error(error));
+  page++;
+}
+
+//pour la navigation des caroussels
+const nextBtn = document.getElementById('next-btn');
+const prevBtn = document.getElementById('prev-btn');
+
+nextBtn.addEventListener('click', function() {
+  navigateFilms(nextBtn);
+});
+
+prevBtn.addEventListener('click', function() {
+  navigateFilms(prevBtn);
+});
+
+function navigateFilms(btn) {
+  const topFilmsList = document.getElementById('top-films-list','top-film-genre');
+  const visibleFilms = document.querySelectorAll('.top-film-img:not(.hidden)');
+  const hiddenFilms = document.querySelectorAll('.top-film-img.hidden');
+  const numVisibleFilms = visibleFilms.length;
+  const numHiddenFilms = hiddenFilms.length;
+
+  if (btn.id === 'next-btn') {
+    if (numHiddenFilms > 0) {
+      for (let i = 0; i < 4; i++) {
+        visibleFilms[i].classList.add('hidden');
+        hiddenFilms[numHiddenFilms - i - 1].classList.remove('hidden');
+      }
+    }
+  } else if (btn.id === 'prev-btn') {
+    if (numVisibleFilms > 4) {
+      const lastVisibleIndex = numVisibleFilms - 1;
+      const firstHiddenIndex = numHiddenFilms - 1;
+      for (let i = 0; i < 4; i++) {
+        if (lastVisibleIndex - i >= 4) {
+          visibleFilms[lastVisibleIndex - i].classList.add('hidden');
+          hiddenFilms[firstHiddenIndex + i].classList.remove('hidden');
+        } else {
+          break;
         }
-      })
-      .catch(error => console.error(error));
-      page++;
+      }
+    }
+  }
 }
 
 
-
+//pour les septs meilleurs films du genre romance
 var page = 1;
 let allFilmsGenre = [];
 while (page < 3)
@@ -86,15 +129,8 @@ while (page < 3)
         allFilmsGenre = allFilmsGenre.concat(topFilmGenre);
         if (allFilmsGenre.length == 10){
             allFilmsGenre = allFilmsGenre.slice(1,8);
-
             var element = document.getElementById("top-films-genre");
-        if (allFilmsGenre.length >= 4){
-
             element.classList.add("active");
-        } else {
-
-            element.classList.remove("active");
-}
             const topFilmGenre = document.getElementById('top-films-genre');
             allFilmsGenre.forEach(film => {
               const filmGenre = document.getElementById('film-category');
@@ -106,13 +142,12 @@ while (page < 3)
               topFilmGenre.appendChild(filmGenre);
             });
         }
-
       })
       .catch(error => console.error(error));
       page++;
 }
 
-
+//pour les septs meilleurs films du genre drama
 var page = 1;
 let allFilmsDrama = [];
 while (page < 3)
@@ -127,13 +162,8 @@ while (page < 3)
             allFilmsDrama = allFilmsDrama.slice(1,8);
 
             var element = document.getElementById("top-films-drama");
-        if (allFilmsDrama.length >= 4){
-
             element.classList.add("active");
-        } else {
 
-            element.classList.remove("active");
-}
 
             const topFilmDrama = document.getElementById('top-films-drama');
             allFilmsDrama.forEach(film => {
@@ -152,34 +182,33 @@ while (page < 3)
       .catch(error => console.error(error));
       page++;
 }
-
-
-
+//pour les septs meilleurs films du genre crime
 var page = 1;
-let allFilmsAction = [];
+let allFilmsCrime = [];
 while (page < 3)
 {
-    fetch('http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=Action&page='+page, {async: false})
+    fetch('http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=Crime&page='+page, {async: false})
       .then(response => response.json())
       .then(data => {
-        let topFilmAction = data.results;
+        let topFilmCrime = data.results;
 
-        allFilmsAction = allFilmsAction.concat(topFilmAction);
-        if (allFilmsAction.length == 10){
-            allFilmsAction = allFilmsAction.slice(1,8);
-            var element = document.getElementById("top-films-action");
+        allFilmsCrime = allFilmsCrime.concat(topFilmCrime);
+        if (allFilmsCrime.length == 10){
+            allFilmsCrime = allFilmsCrime.slice(1,8);
+
+            var element = document.getElementById("top-films-crime");
             element.classList.add("active");
-            const topFilmAction = document.getElementById('top-films-action');
 
-            allFilmsAction.forEach(film => {
-              const filmAction = document.getElementById('film-action');
-              filmAction.innerHTML += `
+            const topFilmCrime = document.getElementById('top-films-crime');
+            allFilmsCrime.forEach(film => {
+              const filmCrime = document.getElementById('film-crime');
+              filmCrime.innerHTML += `
 
                     <div class="top-film-img">
-                        <img src="${film.image_url}" alt="${film.title}" onclick="ouvrirModal('${film.url}')">
+                        <img src="${film.image_url}" alt="${film.title}" onclick="ouvrirModal()">
                     </div>
               `;
-              topFilmAction.appendChild(filmAction);
+              topFilmCrime.appendChild(filmCrime);
             });
         }
 
@@ -190,53 +219,20 @@ while (page < 3)
 
 
 
+
+
+
 // Récupérer la div de la fenêtre modale
 const modal = document.getElementById("myModal");
 
-// Récupérer le bouton de lecture
-//const playBtn = document.querySelector(".play-button");
-
-// Ajouter un événement "click" au bouton de lecture
-//playBtn.addEventListener("click", ouvrirModal);
 // Récupérer le bouton de fermeture
 const closeBtn = document.querySelector(".close");
 
 // Ajouter un événement "click" au bouton de fermeture
 closeBtn.addEventListener("click", fermerModal);
 
-
 // Fonction pour ouvrir la fenêtre modale
 
-function testouvrirModal(){
-  // Récupérer les informations du film
-   var modal = document.getElementById("myModal");
-  // Récupération des données depuis l'API
-  fetch('http://localhost:8000/api/v1/titles/?sort_by=-imdb_score')
-  .then(response=> response.json())
-  .then(dataurl=> {
-   var myurl = dataurl.results[0].url;
-   fetch(myurl)
-    .then(response => response.json())
-    .then(data => {
-      // Affichage des données dans le modal
-
-      document.getElementById("modal-title").innerHTML = data.original_title;
-      document.getElementById("modal-genre").innerHTML = "Genre : " + data.genres.join(", ");
-      document.getElementById("modal-release").innerHTML = "Date de sortie : " + data.year;
-      document.getElementById("modal-rated").innerHTML = "Classification : " + data.rated;
-      document.getElementById("modal-imdb").innerHTML = "IMDb : " + data.imdb_score;
-      document.getElementById("modal-director").innerHTML = "Réalisateur : " + data.directors.join(", ");
-      document.getElementById("modal-actors").innerHTML = "Acteurs : " + data.actors.join(", ");
-      document.getElementById("modal-duration").innerHTML = "Durée : " + data.duration + " minutes";
-      document.getElementById("modal-country").innerHTML = "Pays : " + data.countries.join(", ");
-      document.getElementById("modal-box-office").innerHTML = "Box-office : " + data.worldwide_gross_income + " $";
-      document.getElementById("modal-summary").innerHTML = "Résumé : " + data.description;
-    })
-    .catch(error => console.log(error));
-  })
-   // Affichage du modal
-  modal.style.display = "block";
-}
 
 function ouvrirModal(myurl){
   // Récupérer les informations du film
@@ -270,6 +266,7 @@ function fermerModal() {
   // Fermeture du modal
   modal.style.display = "none";
 }
+
 
 
 
