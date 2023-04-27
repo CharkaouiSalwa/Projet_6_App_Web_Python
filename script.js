9b0ab3b82e2eac5291c0fdb22e542b44e6fa5a50
CharkaouiSalwa <salwacharkaoui@gmail.com>
2023-04-27 20:09:59 +0200

    ajouter la fonction navigateFilms pour les buttons de navigations du caroussels

--- script.js
+++ script.js
@@ -74,153 +74,96 @@ while (page < 3) {
   page++;
 }
 
-//pour la navigation des caroussels
-const nextBtn = document.getElementById('next-btn');
-const prevBtn = document.getElementById('prev-btn');
-
-nextBtn.addEventListener('click', function() {
-  navigateFilms(nextBtn);
-});
-
-prevBtn.addEventListener('click', function() {
-  navigateFilms(prevBtn);
-});
-
-function navigateFilms(btn) {
-  const topFilmsList = document.getElementById('top-films-list','top-film-genre');
-  const visibleFilms = document.querySelectorAll('.top-film-img:not(.hidden)');
-  const hiddenFilms = document.querySelectorAll('.top-film-img.hidden');
-  const numVisibleFilms = visibleFilms.length;
-  const numHiddenFilms = hiddenFilms.length;
-
-  if (btn.id === 'next-btn') {
-    if (numHiddenFilms > 0) {
-      for (let i = 0; i < 4; i++) {
-        visibleFilms[i].classList.add('hidden');
-        hiddenFilms[numHiddenFilms - i - 1].classList.remove('hidden');
-      }
-    }
-  } else if (btn.id === 'prev-btn') {
-    if (numVisibleFilms > 4) {
-      const lastVisibleIndex = numVisibleFilms - 1;
-      const firstHiddenIndex = numHiddenFilms - 1;
-      for (let i = 0; i < 4; i++) {
-        if (lastVisibleIndex - i >= 4) {
-          visibleFilms[lastVisibleIndex - i].classList.add('hidden');
-          hiddenFilms[firstHiddenIndex + i].classList.remove('hidden');
-        } else {
-          break;
-        }
-      }
-    }
-  }
-}
 
 
 //pour les septs meilleurs films du genre romance
-var page = 1;
 let allFilmsGenre = [];
-while (page < 3)
-{
-    fetch('http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=Romance&page='+page, {async: false})
-      .then(response => response.json())
-      .then(data => {
-        let topFilmGenre = data.results;
-        allFilmsGenre = allFilmsGenre.concat(topFilmGenre);
-        if (allFilmsGenre.length == 10){
-            allFilmsGenre = allFilmsGenre.slice(1,8);
-            var element = document.getElementById("top-films-genre");
-            element.classList.add("active");
-            const topFilmGenre = document.getElementById('top-films-genre');
-            allFilmsGenre.forEach(film => {
-              const filmGenre = document.getElementById('film-category');
-              filmGenre.innerHTML += `
-                    <div class="top-film-img">
-                        <img src="${film.image_url}" alt="${film.title}" onclick="ouvrirModal('${film.url}')">
-                    </div>
-              `;
-              topFilmGenre.appendChild(filmGenre);
-            });
-        }
-      })
-      .catch(error => console.error(error));
-      page++;
+var page = 1;
+while (page < 3) {
+  fetch(`http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=Romance&page=${page}`)
+    .then(response => response.json())
+    .then(data => {
+      const topFilmGenre = document.getElementById('top-films-genre');
+      data.results.forEach(film => {
+        const filmGenre = document.createElement('div');
+        filmGenre.classList.add('top-film-img-gnr');
+        filmGenre.innerHTML = `
+          <img src="${film.image_url}" alt="${film.title}" onclick="ouvrirModal('${film.url}')">
+        `;
+        allFilmsGenre.push(filmGenre);
+      });
+      if (allFilmsGenre.length >= 7) {
+        allFilmsGenre = allFilmsGenre.slice(0, 7);
+        allFilmsGenre.forEach((film, index) => {
+          if (index >= 4) {
+            film.classList.add('hidden');
+          }
+          topFilmGenre.appendChild(film);
+        });
+      }
+    })
+    .catch(error => console.error(error));
+  page++;
 }
-
 //pour les septs meilleurs films du genre drama
-var page = 1;
 let allFilmsDrama = [];
-while (page < 3)
-{
-    fetch('http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=Drama&page='+page, {async: false})
-      .then(response => response.json())
-      .then(data => {
-        let topFilmDrama = data.results;
-
-        allFilmsDrama = allFilmsDrama.concat(topFilmDrama);
-        if (allFilmsDrama.length == 10){
-            allFilmsDrama = allFilmsDrama.slice(1,8);
-
-            var element = document.getElementById("top-films-drama");
-            element.classList.add("active");
-
-
-            const topFilmDrama = document.getElementById('top-films-drama');
-            allFilmsDrama.forEach(film => {
-              const filmDrama = document.getElementById('film-drama');
-              filmDrama.innerHTML += `
-
-                    <div class="top-film-img">
-                        <img src="${film.image_url}" alt="${film.title}" onclick="ouvrirModal()">
-                    </div>
-              `;
-              topFilmDrama.appendChild(filmDrama);
-            });
-        }
-
-      })
-      .catch(error => console.error(error));
-      page++;
-}
-//pour les septs meilleurs films du genre crime
 var page = 1;
-let allFilmsCrime = [];
-while (page < 3)
-{
-    fetch('http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=Crime&page='+page, {async: false})
-      .then(response => response.json())
-      .then(data => {
-        let topFilmCrime = data.results;
-
-        allFilmsCrime = allFilmsCrime.concat(topFilmCrime);
-        if (allFilmsCrime.length == 10){
-            allFilmsCrime = allFilmsCrime.slice(1,8);
-
-            var element = document.getElementById("top-films-crime");
-            element.classList.add("active");
-
-            const topFilmCrime = document.getElementById('top-films-crime');
-            allFilmsCrime.forEach(film => {
-              const filmCrime = document.getElementById('film-crime');
-              filmCrime.innerHTML += `
-
-                    <div class="top-film-img">
-                        <img src="${film.image_url}" alt="${film.title}" onclick="ouvrirModal()">
-                    </div>
-              `;
-              topFilmCrime.appendChild(filmCrime);
-            });
-        }
-
-      })
-      .catch(error => console.error(error));
-      page++;
+while (page < 3) {
+  fetch(`http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=Drama&page=${page}`)
+    .then(response => response.json())
+    .then(data => {
+      const topFilmDrama = document.getElementById('top-films-drama');
+      data.results.forEach(film => {
+        const filmDrama = document.createElement('div');
+        filmDrama.classList.add('top-film-img-dr');
+        filmDrama.innerHTML = `
+          <img src="${film.image_url}" alt="${film.title}" onclick="ouvrirModal('${film.url}')">
+        `;
+        allFilmsDrama.push(filmDrama);
+      });
+      if (allFilmsDrama.length >= 7) {
+        allFilmsDrama = allFilmsDrama.slice(0, 7);
+        allFilmsDrama.forEach((film, index) => {
+          if (index >= 4) {
+            film.classList.add('hidden');
+          }
+          topFilmDrama.appendChild(film);
+        });
+      }
+    })
+    .catch(error => console.error(error));
+  page++;
 }
 
-
-
-
-
+//pour les septs meilleurs films du genre crime
+let allFilmsCrime = [];
+var page = 1;
+while (page < 3) {
+  fetch(`http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&genre=Crime&page=${page}`)
+    .then(response => response.json())
+    .then(data => {
+      const topFilmCrime = document.getElementById('top-films-crime');
+      data.results.forEach(film => {
+        const filmCrime = document.createElement('div');
+        filmCrime.classList.add('top-film-img-cr');
+        filmCrime.innerHTML = `
+          <img src="${film.image_url}" alt="${film.title}" onclick="ouvrirModal('${film.url}')">
+        `;
+        allFilmsCrime.push(filmCrime);
+      });
+      if (allFilmsCrime.length >= 7) {
+        allFilmsCrime = allFilmsCrime.slice(0, 7);
+        allFilmsCrime.forEach((film, index) => {
+          if (index >= 4) {
+            film.classList.add('hidden');
+          }
+          topFilmCrime.appendChild(film);
+        });
+      }
+    })
+    .catch(error => console.error(error));
+  page++;
+}
 
 // Récupérer la div de la fenêtre modale
 const modal = document.getElementById("myModal");
@@ -267,6 +210,163 @@ function fermerModal() {
   modal.style.display = "none";
 }
 
+//pour la navigation des caroussels
+const nextBtn = document.getElementById('next-btn');
+const prevBtn = document.getElementById('prev-btn');
 
+nextBtn.addEventListener('click', function() {
+  navigateFilms(nextBtn);
+});
 
+prevBtn.addEventListener('click', function() {
+  navigateFilms(prevBtn);
+});
 
+const suivBtn = document.getElementById('suiv-btn');
+const precBtn = document.getElementById('prec-btn');
+
+suivBtn.addEventListener('click', function() {
+  navigateFilms(suivBtn);
+});
+
+precBtn.addEventListener('click', function() {
+  navigateFilms(precBtn);
+});
+
+const suivBtn1 = document.getElementById('suiv-btn-1');
+const precBtn1 = document.getElementById('prec-btn-1');
+
+suivBtn1.addEventListener('click', function() {
+  navigateFilms(suivBtn1);
+});
+
+precBtn1.addEventListener('click', function() {
+  navigateFilms(precBtn1);
+});
+
+const suivBtn2 = document.getElementById('suiv-btn-2');
+const precBtn2 = document.getElementById('prec-btn-2');
+
+suivBtn2.addEventListener('click', function() {
+  navigateFilms(suivBtn2);
+});
+
+precBtn2.addEventListener('click', function() {
+  navigateFilms(precBtn2);
+});
+
+function navigateFilms(btn) {
+  const topFilmsList = document.getElementById('top-films-list');
+  const visibleFilms = document.querySelectorAll('.top-film-img:not(.hidden)');
+  const hiddenFilms = document.querySelectorAll('.top-film-img.hidden');
+   const numVisibleFilms = visibleFilms.length;
+  const numHiddenFilms = hiddenFilms.length;
+
+  const topFilmsGenre = document.getElementById('top-films-genre');
+  const visibleFilmsGnr = document.querySelectorAll('.top-film-img-gnr:not(.hidden)');
+  const hiddenFilmsGnr = document.querySelectorAll('.top-film-img-gnr.hidden');
+  const numVisibleFilmsGnr = visibleFilmsGnr.length;
+  const numHiddenFilmsGnr = hiddenFilmsGnr.length;
+
+  const topFilmsDr = document.getElementById('top-films-drama');
+  const visibleFilmsDr = document.querySelectorAll('.top-film-img-dr:not(.hidden)');
+  const hiddenFilmsDr = document.querySelectorAll('.top-film-img-dr.hidden');
+  const numVisibleFilmsDr = visibleFilmsDr.length;
+  const numHiddenFilmsDr = hiddenFilmsDr.length;
+
+  const topFilmsCr = document.getElementById('top-films-crime');
+  const visibleFilmsCr = document.querySelectorAll('.top-film-img-cr:not(.hidden)');
+  const hiddenFilmsCr = document.querySelectorAll('.top-film-img-cr.hidden');
+  const numVisibleFilmsCr = visibleFilmsCr.length;
+  const numHiddenFilmsCr = hiddenFilmsCr.length;
+
+  if (btn.id === 'next-btn') {
+    if (numHiddenFilms > 0) {
+      for (let i = 0; i < 4; i++) {
+        if (hiddenFilms[i]) {
+          hiddenFilms[i].classList.remove('hidden');
+        }
+        if (visibleFilms[i]) {
+          visibleFilms[i].classList.add('hidden');
+        }
+      }
+    }
+  } else if (btn.id === 'prev-btn') {
+    if (numVisibleFilms >= 3) {
+      for (let i = 1; i <= 4; i++) {
+        if (visibleFilms[numVisibleFilms - i]) {
+          visibleFilms[numVisibleFilms - i].classList.add('hidden');
+        }
+        if (hiddenFilms[numHiddenFilms - i]) {
+          hiddenFilms[numHiddenFilms - i].classList.remove('hidden');
+        }
+      }
+    }
+  } else if (btn.id === 'suiv-btn') {
+    if (numHiddenFilmsGnr > 0) {
+      for (let i = 0; i < 4; i++) {
+        if (hiddenFilmsGnr[i]) {
+          hiddenFilmsGnr[i].classList.remove('hidden');
+        }
+        if (visibleFilmsGnr[i]) {
+          visibleFilmsGnr[i].classList.add('hidden');
+        }
+      }
+    }
+  }else if (btn.id === 'prec-btn') {
+    if (numVisibleFilmsGnr >= 3) {
+      for (let i = 1; i <= 4; i++) {
+        if (visibleFilmsGnr[numVisibleFilmsGnr - i]) {
+          visibleFilmsGnr[numVisibleFilmsGnr - i].classList.add('hidden');
+        }
+        if (hiddenFilmsGnr[numHiddenFilmsGnr - i]) {
+          hiddenFilmsGnr[numHiddenFilmsGnr - i].classList.remove('hidden');
+        }
+      }
+    }
+}else if (btn.id === 'suiv-btn-1') {
+    if (numHiddenFilmsDr > 0) {
+      for (let i = 0; i < 4; i++) {
+        if (hiddenFilmsDr[i]) {
+          hiddenFilmsDr[i].classList.remove('hidden');
+        }
+        if (visibleFilmsDr[i]) {
+          visibleFilmsDr[i].classList.add('hidden');
+        }
+      }
+    }
+  }else if (btn.id === 'prec-btn-1') {
+    if (numVisibleFilmsDr >= 3) {
+      for (let i = 1; i <= 4; i++) {
+        if (visibleFilmsDr[numVisibleFilmsDr - i]) {
+          visibleFilmsDr[numVisibleFilmsDr - i].classList.add('hidden');
+        }
+        if (hiddenFilmsDr[numHiddenFilmsDr - i]) {
+          hiddenFilmsDr[numHiddenFilmsDr - i].classList.remove('hidden');
+        }
+      }
+    }
+  }else if (btn.id === 'suiv-btn-2') {
+    if (numHiddenFilmsCr > 0) {
+      for (let i = 0; i < 4; i++) {
+        if (hiddenFilmsCr[i]) {
+          hiddenFilmsCr[i].classList.remove('hidden');
+        }
+        if (visibleFilmsCr[i]) {
+          visibleFilmsCr[i].classList.add('hidden');
+        }
+      }
+    }
+  }else if (btn.id === 'prec-btn-2') {
+    if (numVisibleFilmsCr >= 3) {
+      for (let i = 1; i <= 4; i++) {
+        if (visibleFilmsCr[numVisibleFilmsCr - i]) {
+          visibleFilmsCr[numVisibleFilmsCr - i].classList.add('hidden');
+        }
+        if (hiddenFilmsCr[numHiddenFilmsCr - i]) {
+          hiddenFilmsCr[numHiddenFilmsCr - i].classList.remove('hidden');
+        }
+      }
+    }
+  }
+}
